4ab0bebb9d866b91c96151469385239f88a36a13
Cadris <cadrissilver@gmail.com>
2023-05-14 01:23:34 +0530

    Logger Set

--- src/main/java/com/sayan/mult/Application.java
+++ src/main/java/com/sayan/mult/Application.java
@@ -1,13 +1,30 @@
 package com.sayan.mult;
 
+import java.lang.reflect.InaccessibleObjectException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Hello world!
  *
  */
 public class Application 
 {
+	
+	private static Log log = LogFactory.getLog(Application.class);
+	
     public static void main( String[] args )
     {
+    	log.debug("Hello World");
         System.out.println( "Hello World!" ); //NOSONAR
+        
+        try {
+        	throw new InaccessibleObjectException("This is the exception message");
+        } catch (Exception e) {
+			log.error("An Error Occured: ", e);
+		}
+        
+        System.out.println("Data OUT"); //NOSONAR
     }
 }
